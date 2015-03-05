480c24c9ed6f7072fb8909e7e571189f18b3c85d
野村直弘 <naohiro@nomuranaohiro-no-MacBook-Pro.local>
2015-03-06 03:31:19 +0900

    仮想パッドのプレハブ、ソースを追加
    プレハブをHierarchyに持っていけば使えます。
    キャラクターなどを仮想パッドで操作したい時は
    VirtualPad.axisや、VirtualPad.directionを速度に代入してもらえれば操作できるようになります。

---
+++ PadStand.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PadStand : MonoBehaviour {
+
+    GameObject virtualPad;
+	void Start () {
+        virtualPad = GameObject.Find("Pad");
+	}
+	
+	void Update () {
+        transform.position = virtualPad.GetComponent<VirtualPad>().GetPadStandPos();
+	}
+}
---
+++ VirtualPad.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VirtualPad : Mover {
+
+    private bool touchTrigger = false;
+    private Vector3 standardPos;
+	public static Vector3 axis, direction;
+    public Vector3 GetPadStandPos () { return standardPos; }
+
+	void Update () {
+        transform.position = pos;
+        float r = (50.0f*1.8f)*1.25f;
+        axis = direction = Vector3.zero;
+        //if (Input.GetMouseButtonDown (0) && Length(pos - Input.mousePosition) < r)
+        if (Input.GetMouseButtonDown (0))
+        {
+            touchTrigger = true;
+            standardPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y+0.01f, 0);
+        }
+        else if (Input.GetMouseButtonUp (0))
+        {
+            touchTrigger = false;
+        }
+        if (touchTrigger)
+        {
+            pos = Input.mousePosition;
+            Vector3 leng = pos - standardPos;
+            float l = Length (leng);
+            if (r < l) { pos = standardPos + leng * (r / l); }
+            axis = (Normalize (pos - standardPos) / r) * Mathf.Min (r, l);
+            direction = Normalize (pos - standardPos);
+        }
+        else
+        {
+            //pos = Vector3.Lerp (pos, standardPos, 0.2f);
+            pos = standardPos = new Vector3(Screen.width * 10, Screen.height * 10, 0);
+        }
+    }
+}
